d3ed913ed2b4aaf17da94b8d614e1029e68a8973
mybsow <banousow@gmail.com>
2026-02-04 20:08:57 +0100

    Update __init__.py

--- blueprints/legal/__init__.py
+++ blueprints/legal/__init__.py
@@ -1,13 +1,8 @@
-"""
-Blueprint pour les pages légales
-"""
-
 from flask import Blueprint
 
-legal_bp = Blueprint(
-    'legal',
-    __name__,
-    template_folder='../../templates/legal'
-)
+legal_bp = Blueprint('legal', __name__, 
+                    template_folder='../templates',
+                    static_folder='../static')
 
+# Import des routes
 from . import routes
