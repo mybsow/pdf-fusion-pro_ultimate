b6b5cccb73a4b63f82e069ab575340ddf90fb133
mybsow <banousow@gmail.com>
2026-02-08 11:28:56 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,18 @@
-buildCommand: ./build.sh
+#!/bin/bash
+# build.sh
+set -ex
+
+echo "🚀 BUILD SCRIPT"
+
+# Désactiver Poetry
+export POETRY_VIRTUALENVS_CREATE=false
+
+# Installer Tesseract
+apt-get update
+apt-get install -y tesseract-ocr tesseract-ocr-fra poppler-utils
+
+# Packages Python
+pip install --upgrade pip
+pip install -r requirements.txt
+
+echo "✅ BUILD COMPLETE"
