e4e0f25aef9c8fd005be726bb959051d35cb9208
mybsow <banousow@gmail.com>
2026-02-17 15:08:29 +0100

    Update routes.py

--- blueprints/pdf/routes.py
+++ blueprints/pdf/routes.py
@@ -14,7 +14,6 @@ from flask import (
     request,
     jsonify,
     send_file,
-    flash,
     redirect,
     url_for,
     after_this_request,
@@ -34,17 +33,6 @@ from .engine import PDFEngine
 
 # Initialiser dossiers
 AppConfig.initialize()
-
-# Pour les textes statiques
-flash(_('Votre fichier a été converti avec succès'))
-
-# Pour les textes avec variables
-flash(_('Bienvenue %(username)s', username=user.name))
-
-# Pour les formulaires (évaluation tardive)
-class ContactForm(FlaskForm):
-    name = StringField(_l('Nom'), validators=[DataRequired()])
-    email = StringField(_l('Email'), validators=[DataRequired(), Email()])
 
 
 # ============================================================
