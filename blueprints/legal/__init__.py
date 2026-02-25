57e40212375ddfbdc931d371a68436b84c0c71b7
mybsow <banousow@gmail.com>
2026-02-25 13:42:54 +0100

    Update __init__.py

--- blueprints/legal/__init__.py
+++ blueprints/legal/__init__.py
@@ -1,10 +1,13 @@
 from flask import Blueprint
+from pathlib import Path
+
+BASE_DIR = Path(__file__).parent.parent  # remonte jusqu’au dossier racine du projet
 
 legal_bp = Blueprint(
     'legal',
     __name__,
-    template_folder='../../templates',   # IMPORTANT
-    static_folder='../../static'
+    template_folder=str(BASE_DIR / 'templates'),
+    static_folder=str(BASE_DIR / 'static')
 )
 
 from . import routes
